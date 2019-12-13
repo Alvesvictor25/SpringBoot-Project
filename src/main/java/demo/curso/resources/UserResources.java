d5ab60f67c0c464ab0f6f8cd4c3d91b09be01e63
alvesvictor@hotmail.com <alvesvictor-@hotmail.com>
2019-12-13 04:32:55 -0200

    Projeto criado

---
+++ src/main/java/demo/curso/resources/UserResources.java
@@ -0,0 +1,19 @@
+package demo.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import demo.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "1234", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
